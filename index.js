b5d3bbf925d4aad0d5ecf3e5ed0a4b56c31db2ad
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-06 11:48:53 -0500

    Build server

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("index here");
+
+const express = require("express");
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.get(`/api/*`, (_req, res) => {
+  res.status(200).json("Cool");
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
