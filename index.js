94b5fc617763cc5c318d8e6085ec39c461b05f75
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-06 12:56:15 -0500

    react app

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
-console.log("index here");
+const path = require("path");
 
 const express = require("express");
 const server = express();
 
 const PORT = process.env.PORT || 5000;
 
+server.use(express.static(path.join(__dirname, "client/build")));
+
 server.use(express.json());
 
 server.get(`/api/*`, (_req, res) => {
   res.status(200).json("Cool");
+});
+
+server.use("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 server.listen(PORT, () => {
