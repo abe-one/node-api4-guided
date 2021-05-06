4dcd333162bf945578276113079eb2a1f2b1d63b
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-06 17:23:36 -0500

    disable client

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-const path = require("path");
+// const path = require("path");
 
 const express = require("express");
 const server = express();
 
 const PORT = process.env.PORT || 5000;
 
-server.use(express.static(path.join(__dirname, "client/build")));
+// server.use(express.static(path.join(__dirname, "client/build")));
 
 server.use(express.json());
 
@@ -14,9 +14,9 @@ server.get(`/api/*`, (_req, res) => {
   res.status(200).json("Cool");
 });
 
-server.use("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+// server.use("*", (_req, res) => {
+// res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
 
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
